29591bf8107ce3b88d66cec3c6a8765611e92038
Sergey Sayamov <dolamroth@mail.ru>
2023-04-03 00:32:19 +0300

    Unified asgi.py, updated requirements, push version to 0.1.1

--- asgi.py
+++ asgi.py
@@ -1,17 +1,18 @@
-import os
-import sys
-import uvicorn
-
 from starlette_web.common.app import get_app
 
 
+app = get_app()
+
+
 if __name__ == "__main__":
+    import os
+    import sys
+    import uvicorn
+
     settings_module = "starlette_web.tests.settings"
     for arg in sys.argv:
         if arg.startswith("--settings="):
             settings_module = arg[11:]
 
     os.environ.setdefault("STARLETTE_SETTINGS_MODULE", settings_module)
-
-    app = get_app()
     uvicorn.run(app, host="127.0.0.1", port=80)
