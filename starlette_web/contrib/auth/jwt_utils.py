62fdda429403de3d2458a6e8a37c387dac04c56e
Sergey Sayamov <dolamroth@mail.ru>
2023-12-25 12:18:13 +0300

    Minor refactoring of JWTProcessor

--- starlette_web/contrib/auth/jwt_utils.py
+++ starlette_web/contrib/auth/jwt_utils.py
@@ -1,5 +1,6 @@
 import copy
 import datetime
+from functools import cached_property
 from typing import Tuple
 
 import jwt
@@ -16,7 +17,7 @@ class JWTProcessor:
         **kwargs,
     ) -> Tuple[str, datetime.datetime]:
         _expires_at = self._get_expires_at(
-            expires_in_base=expires_in,
+            expires_in=expires_in,
             **self.init_options,
             **kwargs,
         )
@@ -30,7 +31,7 @@ class JWTProcessor:
 
         token = jwt.encode(
             payload=_payload,
-            key=self._get_encode_secret_key(**self.init_options, **kwargs),
+            key=self._get_encode_secret_key,
             **self._get_encode_options(**self.init_options, **kwargs),
         )
         return token, _expires_at
@@ -38,24 +39,45 @@ class JWTProcessor:
     def decode_jwt(self, encoded_jwt: str, **kwargs):
         return jwt.decode(
             encoded_jwt,
-            key=self._get_decode_secret_key(**self.init_options, **kwargs),
+            key=self._get_decode_secret_key,
             **self._get_decode_options(**self.init_options, **kwargs),
         )
 
-    def _get_encode_secret_key(self, **kwargs):
+    @cached_property
+    def _get_encode_secret_key(self):
         raise NotImplementedError()
 
-    def _get_decode_secret_key(self, **kwargs):
+    @cached_property
+    def _get_decode_secret_key(self):
         raise NotImplementedError()
 
-    def _get_expires_at(self, expires_in_base: int = None, **kwargs) -> datetime.datetime:
-        return datetime.datetime.utcnow() + datetime.timedelta(seconds=expires_in_base)
+    def _get_expires_at(self, expires_in: int = None, **kwargs) -> datetime.datetime:
+        return datetime.datetime.utcnow() + datetime.timedelta(seconds=expires_in)
 
     def _enhance_payload_for_encode(self, payload: dict, **kwargs) -> None:
         pass
 
     def _get_encode_options(self, **kwargs) -> dict:
-        return {}
+        res = {}
+        options = {**self.init_options, **kwargs}
+
+        if "algorithm" in options:
+            res["algorithm"] = options["algorithm"]
+
+        return res
 
     def _get_decode_options(self, **kwargs) -> dict:
-        return {}
+        res = {}
+        options = {**self.init_options, **kwargs}
+
+        if "algorithm" in options:
+            res["algorithms"] = [options["algorithm"]]
+        elif "algorithms" in options:
+            res["algorithms"] = options["algorithms"]
+
+        if "options" in options:
+            res["options"] = options["options"]
+        else:
+            res["options"] = options
+
+        return res
--- starlette_web/contrib/auth/utils.py
+++ starlette_web/contrib/auth/utils.py
@@ -1,5 +1,6 @@
 import datetime
 from dataclasses import dataclass
+from functools import cached_property
 
 from starlette_web.common.conf import settings
 from starlette_web.contrib.auth.jwt_utils import JWTProcessor
@@ -19,38 +20,33 @@ class TokenCollection:
 
 
 class AuthJWTProcessor(JWTProcessor):
-    def _get_encode_secret_key(self, **kwargs):
+    @cached_property
+    def _get_encode_secret_key(self):
         return str(settings.SECRET_KEY)
 
-    def _get_decode_secret_key(self, **kwargs):
+    @cached_property
+    def _get_decode_secret_key(self):
         return str(settings.SECRET_KEY)
 
-    def _get_expires_at(self, expires_in_base: int = None, **kwargs) -> datetime.datetime:
+    def _get_expires_at(self, expires_in: int = None, **kwargs) -> datetime.datetime:
         token_type: str = kwargs.get("token_type", TOKEN_TYPE_ACCESS)
 
         if token_type == TOKEN_TYPE_REFRESH:
             expires_in = settings.AUTH_JWT_REFRESH_EXPIRES_IN
         else:
-            expires_in = expires_in_base or settings.AUTH_JWT_EXPIRES_IN
+            expires_in = expires_in or settings.AUTH_JWT_EXPIRES_IN
 
-        return super()._get_expires_at(expires_in_base=expires_in)
+        return super()._get_expires_at(expires_in=expires_in)
 
     def _enhance_payload_for_encode(self, payload: dict, **kwargs) -> None:
         token_type: str = kwargs.get("token_type", TOKEN_TYPE_ACCESS)
         payload["exp_iso"] = payload["exp"].isoformat()
         payload["token_type"] = token_type
 
-    def _get_encode_options(self, **kwargs) -> dict:
-        return {
-            "algorithm": settings.AUTH_JWT_ALGORITHM,
-        }
 
-    def _get_decode_options(self, **kwargs) -> dict:
-        return {
-            "algorithms": [settings.AUTH_JWT_ALGORITHM],
-        }
-
-
-jwt_processor = AuthJWTProcessor()
+jwt_processor = AuthJWTProcessor(
+    algorithm=settings.AUTH_JWT_ALGORITHM,
+    verify_signature=True,
+)
 encode_jwt = jwt_processor.encode_jwt
 decode_jwt = jwt_processor.decode_jwt
