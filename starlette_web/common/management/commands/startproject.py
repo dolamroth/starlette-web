210381508fad8988253e47cd3cf2d134f5ab40fe
aleks <alekseyparputs@gmail.com>
2024-01-08 05:31:56 +0300

    feat(conf, commands): add files templates and update command startproject

--- starlette_web/common/management/commands/startproject.py
+++ starlette_web/common/management/commands/startproject.py
@@ -2,6 +2,7 @@ import os
 import shutil
 from pathlib import Path
 
+import starlette_web
 from starlette_web.common.management.alembic_mixin import AlembicMixin
 from starlette_web.common.management.base import BaseCommand, CommandError, CommandParser
 from starlette_web.common.utils import get_random_string
@@ -13,13 +14,16 @@ class Command(BaseCommand, AlembicMixin):
 
     def add_arguments(self, parser: CommandParser):
         parser.add_argument("project_name", type=str)
+        parser.add_argument("--advanced_project_template", type=str, default="false")
 
     async def handle(self, **options):
         current_dir = os.getcwd()
         project_name = options["project_name"]
+        is_advanced_project_template = True if options.get("advanced_project_template") == "true" else False
 
         cwd = Path(current_dir)
         project_dir = cwd / project_name
+        files_templates_dir = Path(starlette_web.__path__[0], "common", "conf", "files_templates")
         if project_dir.is_file() or project_dir.is_symlink():
             raise CommandError(
                 details=(
@@ -44,39 +48,45 @@ class Command(BaseCommand, AlembicMixin):
                 project_dir / filename,
             )
 
-        env_template = """
-APP_DEBUG=false
-SITE_URL="https://web.project.com/"
-SECRET_KEY={secret_key}
+        # Setup base directories
+        (project_dir / "static").mkdir()
+        (project_dir / "templates").mkdir()
 
-DB_HOST=localhost
-DB_PORT=5432
-DB_NAME=web_project
-DB_USERNAME=postgres
-DB_PASSWORD=postgres
-DB_ECHO=false
-        """
-
+        # Setup env files
+        env_template_content = self._read_template_file(
+            file_name=Path(files_templates_dir, "env_template.py-tpl"),
+        )
         with open(project_dir / ".env", "wt+", encoding="utf-8") as file:
-            content = env_template.format(
+            content = env_template_content.format(
                 secret_key=get_random_string(50),
             )
             file.writelines(content.strip() + "\n")
 
         with open(project_dir / ".env.template", "wt+", encoding="utf-8") as file:
-            content = env_template.format(
+            content = env_template_content.format(
                 secret_key="",
             )
             file.writelines(content.strip() + "\n")
 
-        # Setup base directories
-
-        (project_dir / "static").mkdir()
-        (project_dir / "templates").mkdir()
-
         # Setup alembic
         os.chdir(project_dir)
         await self.run_alembic_main(["init", "-t", "async", self._alembic_directory_name])
+        if is_advanced_project_template:
+            await self._setup_advanced_alembic_conf(
+                project_dir=project_dir,
+                alembic_env_template=Path(files_templates_dir, "alembic_env_template.py-tpl"),
+            )
+        else:
+            await self._setup_default_alembic_conf(project_dir=project_dir)
+
+    def _read_template_file(self, file_name: Path) -> str:
+        if not (file_name.exists() and file_name.is_file()):
+            raise CommandError(details=f"Invalid file template path: {str(file_name)}")
+
+        with file_name.open() as file:
+            return file.read()
+
+    async def _setup_default_alembic_conf(self, project_dir: Path) -> None:
         with open(project_dir / self._alembic_directory_name / "env.py", "rt") as file:
             lines = []
             for line in file:
@@ -98,6 +108,24 @@ DB_ECHO=false
             for line in file:
                 if "# file_template = " in line:
                     lines.append(line[2:])
+                else:
+                    lines.append(line)
+
+        with open(project_dir / "alembic.ini", "wt") as file:
+            file.writelines(lines)
+
+    async def _setup_advanced_alembic_conf(self, project_dir: Path, alembic_env_template: Path) -> None:
+        alembic_env_template_content = self._read_template_file(file_name=alembic_env_template)
+        with open(project_dir / self._alembic_directory_name / "env.py", "wt") as file:
+            file.write(alembic_env_template_content)
+
+        with open(project_dir / "alembic.ini", "rt") as file:
+            lines = []
+            for line in file:
+                if "# file_template = " in line:
+                    lines.append(line[2:])
+                elif "# revision_environment = false" in line:
+                    lines += "revision_environment = true"
                 else:
                     lines.append(line)
 
